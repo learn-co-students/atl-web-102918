0e960b86aa2e74c521c61f086b68fecdd81e36b1
kingcons <brit@kingcons.io>
2019-01-29 12:16:33 -0500

    Add proper secret management.

--- 38-redux-shelter/restcue-api/app/controllers/application_controller.rb
+++ 38-redux-shelter/restcue-api/app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::API
   def current_user
     token = request.headers['Access-Token']
-    payload = JWT.decode(token, ENV['SECRET'])[0]
+    payload = JWT.decode(token, ENV['APP_SECRET'])[0]
     user_id = payload['user_id']
     user_id && User.find_by(id: user_id)
   rescue JWT::DecodeError
--- 38-redux-shelter/restcue-api/app/controllers/login_controller.rb
+++ 38-redux-shelter/restcue-api/app/controllers/login_controller.rb
@@ -2,7 +2,7 @@ class LoginController < ApplicationController
   def login
     @user = User.find_by(email: params[:email])
     if @user && @user.authenticate(params[:password])
-      token = JWT.encode({ user_id: @user.id }, ENV['SECRET'], 'HS256')
+      token = JWT.encode({ user_id: @user.id }, ENV['APP_SECRET'], 'HS256')
       render json: { token: token, name: @user.name }, status: :ok
     else
       render json: { errors: "Could not login with those credentials" },
