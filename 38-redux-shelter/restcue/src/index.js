a407984d1f2180338ad082c48e66f3344cccb155
kingcons <brit@kingcons.io>
2019-01-29 10:47:36 -0500

    Add redux and react redux and create a store.

--- 38-redux-shelter/restcue/src/index.js
+++ 38-redux-shelter/restcue/src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import '../node_modules/semantic-ui-css/semantic.min.css'
 import App from './App';
+import store from './store';
 import { BrowserRouter } from 'react-router-dom';
-
+import { Provider } from 'react-redux';
 
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<BrowserRouter>
-    <App />
+    <Provider store={store} >
+      <App />
+    </Provider>
   </BrowserRouter>,
   document.getElementById('root'));
 
